56fd78ade2a992672d5d075bc60bb27c99642141
Alucardo18 <emmanuel2464@gmail.com>
2018-04-02 21:26:44 -0500

    fixes and updates

--- intelibot_R1/prueba_sensores_de_obstaculos/prueba_sensores_de_obstaculos.ino
+++ intelibot_R1/prueba_sensores_de_obstaculos/prueba_sensores_de_obstaculos.ino
@@ -1,10 +1,16 @@
+
+
 // Ultrasonic - Library for HR-SC04 Ultrasonic Ranging Module.
 // Rev.4 (06/2012)
 // J.Rodrigo ( www.jra.so )
 // more info at www.ardublog.com
 
 #include <Ultrasonic.h>
+#include "pitches.h"
 
+
+
+//
 Ultrasonic ultrasonic(4,2); // (Trig PIN,Echo PIN)
 int sensor1_Valor = 0;
 int sensor2_Valor = 0;
@@ -13,6 +19,7 @@ int sensor2_pin = 1;
 
 void setup() {
   Serial.begin(9600); 
+  
 
 
 }
@@ -21,14 +28,21 @@ void loop()
 {
   sensor1_Valor = analogRead(sensor1_pin);
   sensor2_Valor = analogRead(sensor2_pin);
-  Serial.print("Distancia sensor ultrasonico: ");
-  Serial.print(ultrasonic.Ranging(CM)); // CM or INC
-  Serial.println(" cm" );
-  Serial.print("Sensor lateral_UNO: ");
-  Serial.println(sensor1_Valor);
-  Serial.print("Sensor lateral_DOS: ");
-  Serial.println(sensor2_Valor);
+  if (ultrasonic.Ranging(CM) <= 10){
+
+  
+  Serial.println("distancia menos a 10 CM");
   Serial.println(" ");
-  delay(200);
+  }
+ 
+Serial.print("Distancia sensor ultrasonico: ");
+ Serial.print(ultrasonic.Ranging(CM)); // CM or INC
+Serial.println(" cm" );
+ Serial.print("Sensor lateral_UNO: ");
+Serial.println(sensor1_Valor);
+Serial.print("Sensor lateral_DOS: ");
+Serial.println(sensor2_Valor);
+ Serial.println(" ");
+ delay(600);
  
 }

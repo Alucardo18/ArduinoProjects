9f19b0420feac5b5afbb3a23b8a4c4225549f1ba
Jorge Cano <jacano.sosa@gmail.com>
2018-12-02 14:20:24 -0600

    Clean the input buffer and the complete flag even when the string is not expected

--- intelibot_R1_BackUp2/intelibot_R1_BackUp2.ino
+++ intelibot_R1_BackUp2/intelibot_R1_BackUp2.ino
@@ -20,7 +20,7 @@ String readString;
 
 void setup() {
 marioCoinTone ();
-  
+
 pinMode(5,OUTPUT);
 pinMode(6,OUTPUT);
 pinMode(7,OUTPUT);
@@ -31,14 +31,14 @@ inputString.reserve(200);
 
 void loop() {
 
-  
+
 int RED_LED =11;
 int GREEN_LED = 10;
 int BLUE_LED =9;
 
 /*URRENT MAPPINGS
 
-PWM RGB LED -> 
+PWM RGB LED ->
 
 9 -> BLUE
 10 -> GREEN
@@ -61,67 +61,63 @@ NEW MAPPINGS FOR NEW RGB LED
       String redValue;
       String greenValue;
       String blueValue;
-   
+
         ind1 = value.indexOf(',');
         redValue = value.substring(3,ind1);
         ind2 = value.indexOf(',', ind1+1 );
         greenValue = value.substring(ind1+1, ind2+1);
         ind3 = value.indexOf(',', ind2+1 );
         blueValue = value.substring(ind2+1, ind3+1);
-        
+
      // Serial.print("redValue = ");   //
-     // Serial.println(redValue.toInt()); 
+     // Serial.println(redValue.toInt());
      // Serial.print("greenValue = ");
      // Serial.println(greenValue.toInt());
      // Serial.print("blueValue = ");
      // Serial.println(blueValue.toInt());
-      
+
       analogWrite(RED_LED,redValue.toInt());
       analogWrite(GREEN_LED,greenValue.toInt());
-      analogWrite(BLUE_LED,blueValue.toInt());      
-           inputString="";
-          stringComplete = false;
-  
+      analogWrite(BLUE_LED,blueValue.toInt());
+      cleanInput();
+
         }
 
           else if (inputString.startsWith("%")){  //CONTROL DE MMOTORES + FUNCIONALIDADES ESPECIALES
              String value = inputString.substring(1);
 
+             if (value.startsWith("STOP")){
+             Serial.print(value);
+              detenido();
+             }
+
                  //CONTROL DE MOTORES
                if (value.startsWith("FORWARD")){
               Serial.print(value);
                adelante();
-              inputString="";
-              stringComplete = false;
               }
-    
-              if (value.startsWith("STOP")){
-              Serial.print(value);
-               detenido();
-              inputString="";
-              stringComplete = false;
-              }
+
                 if (value.startsWith("BACKWARD")){  // no puede ser B por que interfiere con el caracter del color azul "BLUE"
               Serial.print(value);
                atras();
-              inputString="";
-              stringComplete = false;
               }
-  
+
               if (value.startsWith("RIGHT")){   // no puede ser R por que interfiere con el valor del caracter "RED"
               Serial.print(value);
                derecha();
-              inputString="";
-              stringComplete = false;
               }
-              
+
                 if (value.startsWith("LEFT")){
               Serial.print(value);
                izquierda();
-              inputString="";
-              stringComplete = false;
               }
-              
+
+              //
+               if (value.startsWith("HONK")){
+              Serial.print(value);
+              mentada ();
+              }
+
 //////////////////////////////
                   ///MODOS AUTONOMOS
                if (value.startsWith("BATTLE")){
@@ -130,13 +126,11 @@ NEW MAPPINGS FOR NEW RGB LED
               autonomous = true;
                 while (autonomous){
                  battleMode();
-                        if (Serial.available() > 0){ // DETIENE EL LOOP 
+                        if (Serial.available() > 0){ // DETIENE EL LOOP
                             detenido ();
-                          autonomous = false; 
+                          autonomous = false;
                           break;
                         }
-              inputString="";
-              stringComplete = false;
                   }
               }
               //RANGER
@@ -146,44 +140,38 @@ NEW MAPPINGS FOR NEW RGB LED
               autonomous = true;
                 while (autonomous){
                  rangerMode();
-                        if (Serial.available() > 0){ // DETIENE EL LOOP 
+                        if (Serial.available() > 0){ // DETIENE EL LOOP
                             detenido ();
-                          autonomous = false; 
+                          autonomous = false;
                           break;
-                        
+
                         }
-                inputString="";
-                stringComplete = false;
                 }
-              
-              inputString="";
-              stringComplete = false;
+
               }
-              //
-               if (value.startsWith("HONK")){
-              Serial.print(value);
-              mentada ();
-              inputString="";
-              stringComplete = false;
-              }
+
+              cleanInput();
           }
-      
+
       }
 
     }
 
-
+void cleanInput(){
+  inputString="";
+  stringComplete = false;
+}
 
 void serialEvent(){
   while (Serial.available()){
    char inChar = (char)Serial.read();
    inputString += inChar;
-   
+
    if (inChar == '\n'){
-    
+
       stringComplete = true;
-    }    
-  } 
+    }
+  }
 }
 
 
@@ -216,17 +204,17 @@ void adelante (){
   digitalWrite(5,LOW);
   digitalWrite(6,LOW);
   }
- 
-  //MODOS AUTONOMOS
-  
 
-  
+  //MODOS AUTONOMOS
+
+
+
   void rangerMode (){
 sensor1_Valor = analogRead(sensor1_pin);
 sensor2_Valor = analogRead(sensor2_pin);
       adelante();
       if (ultrasonic.Ranging(CM) <= 10){
-      
+
       atras();
       delay(200);
       }
@@ -239,12 +227,12 @@ sensor2_Valor = analogRead(sensor2_pin);
       delay(500);
       }
   }
-  
+
   void battleMode (){
 sensor1_Valor = analogRead(sensor1_pin);
 sensor2_Valor = analogRead(sensor2_pin);
-      
-  
+
+
      if (ultrasonic.Ranging(CM) <= 20){
        adelante();
      }
@@ -259,6 +247,5 @@ sensor2_Valor = analogRead(sensor2_pin);
      else if (ultrasonic.Ranging(CM) > 20){
      detenido();
      }
-    
+
   }
-  
